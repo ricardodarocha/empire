84c2a5b5b4a988375a5910716edce4299dde6df0
Ricardo da Rocha <ricardodarocha@outlook.com>
2023-02-19 13:15:07 -0300

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+#[derive(std::fmt::Debug)]
+enum Aldeao {
+    None,
+    Food(u8),
+    Wood(u8),
+    Gold(u8),
+    Stone(u8),
+}
+
 fn main() {
-    println!("Hello, world!");
+    let aldeao = Aldeao::None;
+    println!("Hello, {:?}!", aldeao);
+    let aldeao = Aldeao::Food(1);
+    println!("Hello, {:?}!", aldeao);
+    let aldeao = Aldeao::Wood(1);
+    println!("Hello, {:?}!", aldeao);
+    let aldeao = Aldeao::Gold(1);
+    println!("Hello, {:?}!", aldeao);
+    let aldeao = Aldeao::Stone(1);
+    println!("Hello, {:?}!", aldeao);
 }
